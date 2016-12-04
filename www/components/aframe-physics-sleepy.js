8983da8f0b7f762037af2881afebdec327459bf0
William Murphy <william@datatitian.com>
2016-12-04 13:39:36 -0600

    improved sleepy init flow for cases where body already initialized

--- www/components/aframe-physics-sleepy.js
+++ www/components/aframe-physics-sleepy.js
@@ -1,32 +1,26 @@
 // Make dynamic bodies idle when not grabbed
 
 AFRAME.registerComponent('sleepy', {
-  // Define component properties.
   schema: {default: 'grabbed'},
   
   dependencies: ['dynamic-body'],
-  /**
-   * Run when component is attached.
-   * @member {Element} el - Entity.
-   * @member data - Component data.
-   */
+
   init: function () {
     this.onDrop = this.onDrop.bind(this);
-    ////if (this.el.hasLoaded) {
-    //  this.initBody();
-    //} else {
-    this.el.addEventListener('body-loaded', this.updateBody.bind(this));
-    //}
+    if (this.el.body) {
+      this.updateBody({body: this.el.body});
+    } else {
+      this.el.addEventListener('body-loaded', this.updateBody.bind(this));
+    }
   },
   
   updateBody: function(evt) {
-    //this.physicsBody = this.el.components['dynamic-body'].body;
+    console.log(this.el.body);
     this.physicsBody = evt.detail.body;
     this.physicsBody.sleep();
   },
   update: function() {
-    //this.physicsBody = this.el.components['dynamic-body'].body;
-    //this.physicsBody.sleep();
+    this.physicsBody = this.el.body;
   },
   
   play: function () {
