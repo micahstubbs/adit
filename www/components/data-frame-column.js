41fc99f383d6dc25d8bf575ca66071d5f43846d4
William Murphy <william@datatitian.com>
2016-12-09 16:21:19 -0600

    data-frame-columns return home after dropping

--- www/components/data-frame-column.js
+++ www/components/data-frame-column.js
@@ -29,19 +29,34 @@ AFRAME.registerComponent('data-frame-column', {
     // define home state for animations
     this.homePos = this.el.getComputedAttribute('position');
     this.homeRot = this.el.getComputedAttribute('rotation');
-    this.anim = document.createElement('a-animation');
+    /*this.anim = document.createElement('a-animation');
     this.anim.setAttribute('attribute', 'position');
     this.anim.setAttribute('to', AFRAME.utils.coordinates.stringify(this.homePos));
-    this.anim.setAttribute('begin', this.data.dropEvent);
+    this.anim.setAttribute('begin', 'never');
     this.el.appendChild(this.anim);
+    this.el.addEventListener('animationstart', function(evt){console.log(evt);});
+    this.el.addEventListener('animationend', function(evt){console.log(evt);});*/
+    this.el.addEventListener('stateremoved', this.returnHome.bind(this));
+    this.el.addEventListener('schemachanged', this.physicsSync.bind(this));
     
   },
   
   update: function () {
-    //this.el.emit('model-loaded');
+    this.homePos = this.el.getComputedAttribute('position');
+    this.homeRot = this.el.getComputedAttribute('rotation');
+
   },
   
-  returnHome: function () {
-    
+  physicsSync: function (evt) {
+    this.components['dynamic-body'].syncToPhysics();
+  },
+  
+  returnHome: function (evt) {
+    if(evt.detail.state == 'grabbed') {
+      this.el.body.sleep();
+      this.el.setAttribute('position', this.homePos);
+      this.el.setAttribute('rotation', this.homeRot);
+      //this.anim.start();
+    }
   }
 });
