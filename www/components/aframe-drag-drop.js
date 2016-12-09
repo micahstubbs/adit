b56c266ac03897ba7f0f4aa9cb2f9334749b3d77
William Murphy <william@datatitian.com>
2016-12-09 10:48:45 -0600

    added stack system to drag-drop to ensure expected results when multiple drop targets overlap

--- www/components/aframe-drag-drop.js
+++ www/components/aframe-drag-drop.js
@@ -4,91 +4,98 @@ AFRAME.registerComponent('drag-drop', {
   // seletor to other controller
   schema: { 
             dropEvent: { default: 'dropped' },
-            colliderState: { default: 'collided'},
-            hoveredState: { default: 'hovered'}
+            colliderState: { default: 'collided' },
+            hoveredState: { default: 'hovered' },
+            targets: { type: 'selectorAll' },
+            targetClass: { default: "hoverable" }  
   },
   
   dependencies: ['grab'],
   
   init: function () {
-    this.hitEl = null;
-    // keep tabs on next item hit when they overlap, so that it can 
-    // be made the active target when the first is exited
-    this.secondHitEl = null;
+    this.hitEls = [];
 
     // Bind event handlers
+    this.unHover = this.unHover.bind(this);
     this.onHit = this.onHit.bind(this);
     this.onGripOpen = this.onGripOpen.bind(this);
-    //this.onGripClose = this.onGripClose.bind(this);
   },
   
 
   play: function () {
     var el = this.el;
     el.addEventListener('hit', this.onHit);
-    //el.addEventListener('gripdown', this.onGripClose);
     el.addEventListener('gripup', this.onGripOpen);
-    //el.addEventListener('trackpaddown', this.onGripClose);
     el.addEventListener('trackpadup', this.onGripOpen);
-  //  el.addEventListener('triggerdown', this.onGripClose);
     el.addEventListener('triggerup', this.onGripOpen);
   },
 
   pause: function () {
     var el = this.el;
     el.removeEventListener('hit', this.onHit);
-    //el.removeEventListener('gripdown', this.onGripClose);
     el.removeEventListener('gripup', this.onGripOpen);
-    //el.removeEventListener('trackpaddown', this.onGripClose);
     el.removeEventListener('trackpadup', this.onGripOpen);
-    //el.removeEventListener('triggerdown', this.onGripClose);
     el.removeEventListener('triggerup', this.onGripOpen);
   },
 
 
   onGripOpen: function (evt) {
-    var hitEl = this.hitEl;
-    var carried = this.carried;
-    if (!hitEl) { return; }
-    hitEl.emit(this.data.dropEvent, carried);
-    carried.emit(this.data.dropEvent, hitEl);
-    hitEl.removeState(this.data.hoveredState);
-    this.hitEl = undefined;
+    var hitEls = this.hitEls.slice();
+    if(hitEls.length !== 0) {
+      var hitEl = hitEls[0]; 
+      var carried = this.carried;
+      hitEl.emit(this.data.dropEvent, { drop: 'receive', el: carried });
+      if (carried) carried.emit(this.data.dropEvent, { drop: 'give', el: hitEl });
+      // clear list of backup targets to prevent triggering hover
+      this.hitEls = [];
+      var hoveredState = this.data.hoveredState;
+      hitEls.forEach(function (x) {
+        x.removeState(hoveredState);
+      });
+    }
     this.carried = undefined;
-    this.secondHitEl = undefined;
   },
 
   onHit: function (evt) {
     var hitEl = evt.detail.el;
     var carried = this.el.components.grab.hitEl;
     // start hover when there is a hit, 
-    // not currently hovering something else, and
+    // the hit has the right class
     // have something grabbed,
     // and the grabbed thing is not the hit thing
-    if (hitEl && carried && hitEl !== carried) {
-      if(!this.hitEl) {
-         hitEl.addState(this.data.hoveredState);
-         this.hitEl = hitEl;
-         this.carried = this.el.components.grab.hitEl;
-         hitEl.addEventListener('stateremoved', this.unhover.bind(this));
-      } else {
-        //console.log("secondary hit", hitEl);
-        this.secondHitEl = hitEl;
+    if (hitEl && hitEl.classList.contains(this.data.targetClass) && 
+        carried && hitEl !== carried) {
+      if (this.hitEls.indexOf(hitEl) === -1) { 
+        this.hitEls.push(hitEl); 
+        hitEl.addEventListener('stateremoved', this.unHover);
+        // only initiate a hover if the pushed item is the first
+        // this keeps from having multiple drop targets when they overlap
+        if (this.hitEls.length === 1) {
+           this.hover();
+        }
       }
     } 
   },
   
-  unhover: function (evt) {
-    if(evt.detail.state == this.data.colliderState) {
-      evt.target.removeEventListener('stateremoved', this.unhover.bind(this));
-      evt.target.removeState(this.data.hoveredState);
-      this.hitEl = undefined;
-      // activate backup target if present
-      if(this.secondHitEl) {
-        //console.log("fire backup")
-        this.onHit({ detail: { el: this.secondHitEl } });
-        this.secondHitEl = undefined;
+  hover: function() {
+    if(this.hitEls.length) {
+      let hoverEl = this.hitEls[0];
+      hoverEl.addState(this.data.hoveredState);
+      this.carried = this.el.components.grab.hitEl;
+    }
+  },
+  
+  unHover: function (evt) {
+    if (evt.detail.state == this.data.colliderState ||
+        evt.detail.state == this.data.hoveredState) {
+      let hoverIndex = this.hitEls.indexOf(evt.target);
+      evt.target.removeEventListener('stateremoved', this.unHover);
+      if (evt.target.is(this.data.hoveredState)) {
+          evt.target.removeState(this.data.hoveredState);
       }
+      if (hoverIndex > -1) { this.hitEls.splice(hoverIndex, 1); } 
+      // activate backup target if present
+      this.hover();
     }
   }
 });
