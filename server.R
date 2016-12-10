397e7eca576412060490216bc5552cea6be24e8b
William Murphy <william@datatitian.com>
2016-12-10 12:00:13 -0600

    simplified reactive schema and fixed scoping issue

--- server.R
+++ server.R
@@ -18,34 +18,7 @@ shinyServer(function(input, output) {
                 z = "Petal.Length",
                 color = "Petal.Width",
                 shape = "Species")
-  # mappings <- c(x = "wt",
-  #               y = "mpg",
-  #               z = "hp")
-  map_message <- reactiveValues()
-  dummy <- reactiveValues(x = 0)
-  mappings_string <- reactive({
-    dummy$x
-    input$mapppings
-    cat("mappings_aes\n")
-    reactiveValuesToList(map_message)
-    if(!is.null(map_message$variable)) {
-      mappings[map_message$mapping] <- map_message$variable
-    }
-    mappings
-  })
-  
-  observeEvent(input$mappings, {
-    lapply(input$mappings, function(x)cat(paste0(x, "\n")))
-    map_message$variable <- input$mappings$variable
-    map_message$mapping <- input$mappings$mapping
-    map_message$chart <- map_message$chart
-    dummy$x <- dummy$x + 1
-    cat(dummy$x, "\n")
-  })
-  # observe({
-  #   mappings <- mappings[mappings %in% names(selected_data())]
-  #   })
-  
+ 
   selected_data <- reactive({
     switch(input$datasource, 
            "iris" = iris,
@@ -61,16 +34,19 @@ shinyServer(function(input, output) {
   output$myplot <- renderAScatter3d({
     # setup plot in ggplot and the aScatter3d widget will translate it into
     # aframe
-    #cat(names(as.character(mappings_aes())), as.character(mappings_aes()))
-    cat("plot", dummy$x, "\n")
-    input$mappings
-    dummy$x
-    mappings <- mappings_string()
-    mappings <- mappings[mappings %in% names(selected_data())]
-    req(length(mappings) > 0)
-    mappings_aes <- do.call(aes_string, as.list(mappings))
-    
-    
+    cat("plot update\n")
+    update <- input$mappings
+    if(!is.null(update$variable)) {
+      cat(paste("mapping update:", update$mapping, update$variable, "\n"))
+      mappings[update$mapping] <<- update$variable
+    }
+    usable_mappings <- mappings[mappings %in% names(selected_data())]
+    cat(paste(names(mappings), mappings, sep = ": "), "\n")
+    cat(usable_mappings[c("x", "y", "z")])
+    req(!anyNA(usable_mappings[c("x", "y", "z")]), 
+        cancelOutput = TRUE)
+    cat("plotting\n")
+    mappings_aes <- do.call(aes_string, as.list(usable_mappings))
     plt <- ggplot(selected_data(), mappings_aes) +
       geom_point()
     aScatter3d(plt)
