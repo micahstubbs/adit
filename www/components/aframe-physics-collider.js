788060d0afe04f6c13c71a9ccbbcc030c6b78ce0
William Murphy <william@datatitian.com>
2016-12-16 20:58:04 -0600

    cleanup physics-collider

--- www/components/aframe-physics-collider.js
+++ www/components/aframe-physics-collider.js
@@ -1,14 +1,28 @@
 AFRAME.registerComponent("physics-collider", {
   schema: {
-    collidedState: { default: 'collided' }
+    collidedState: { default: 'collided' },
+    ignoreSleep: { default: true },
+    collisionPhysics: {default: false }
   },
   init: function() {
-    this.el.addEventListener("body-loaded", (evt) => {
-      evt.detail.target.body.collisionResponse = false;
-      evt.detail.target.body.type = 0;
-      evt.detail.target.body.allowSleep = false;
-    });
     this.collisions = [];
+  },
+  update: function() {
+    if(this.el.body) {
+      this.updateBody();
+    }  else {
+      this.el.addEventListener('body-loaded', this.updateBody.bind(this));
+    }
+  },
+  updateBody: function() {
+    if(this.originalType === undefined) this.originalType = this.el.body.type;
+    // ensure sleep doesn't disable collision detection
+    this.el.body.allowSleep = false; 
+    this.el.body.collisionResponse = this.data.collisionPhysics;
+    /* naiveBroadphase ignores collisions between static or kinematic bodies 
+    and sleeping bodies by default; disable this by assigning an 
+    invalid body type */
+    this.el.body.type = this.data.ignoreSleep ? 0 : this.originalType;
   },
   tick: function() {
     var collisions = [],
