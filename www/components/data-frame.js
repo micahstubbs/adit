22fbdae113f1868eba4e507a9cd19d53118505bd
William Murphy <william@datatitian.com>
2016-12-27 15:44:11 -0600

    Cleanup redundant layout.update call and various commented out code fragments. Tried to eliminate direct call to `dynamic-body`.remove, thinking removeChild would trigger it, but this resulted in a stack overflow error.

--- www/components/data-frame.js
+++ www/components/data-frame.js
@@ -8,32 +8,20 @@ AFRAME.registerComponent('data-frame', {
   },
   
   dependencies: ["position"],
-  /**
-   * Run when component is attached.
-   * @member {Element} el - Entity.
-   * @member data - Component data.
-   */
+
   init: function () {
     this.el.setAttribute("layout", this.data.layout);
-    //this.myCols = [];
   },
   
   update: function() {
     var frameEl = this.el;
     var schemaDat = this.data;
-    /*frameEl.childNodes.forEach(function(col) {
-      if(col.nodeType == Node.ELEMENT_NODE &&
-           col.hasAttribute('data-frame-column')) {
-        frameEl.removeChild(col);
-      }
-    });*/
-    
+
     while(frameEl.lastChild) {
       frameEl.lastChild.components['dynamic-body'].remove();
       frameEl.removeChild(frameEl.lastChild);
     }
 
-    //this.myCols = schemaDat.data;
     schemaDat.data.forEach(function(c) {
       var newCol = document.createElement('a-plane');
       frameEl.appendChild(newCol);
@@ -42,16 +30,12 @@ AFRAME.registerComponent('data-frame', {
       newCol.setAttribute('mixin', schemaDat.columnMixin);
     });
    
-    //this.el.setAttribute("layout", schemaDat.layout);
     
   },
   
   updateData: function(newDat) {
-    //if(this.el.hasAttribute('layout')) this.el.components.layout.remove();
     this.data.data = newDat;
     this.el.components.layout.children = [];
     this.update();
-    this.el.components.layout.update();
-    //this.el.setAttribute('layout', this.data.layout);
   }
 });
